0b1121b5b5b46219cec8ccbcfae54dbc7a66d150
Dineth Wijesuriya <168741233+Dineth-W@users.noreply.github.com>
2026-08-09 19:15:06 +0530

    Enhance display.h with additional utility functions
    Added utility functions for screen display.

--- display.h
+++ display.h
@@ -1,12 +1,21 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <Arduino.h>
+#include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 
 extern Adafruit_ILI9341 tft;
 
+// Initialization
 void displayInit();
-void splashScreen();
+
+// Common screen functions
+void clearScreen();
+void drawSplash();
+
+// Utility functions
+void drawCenteredText(String text, int y, uint16_t color, uint8_t size);
 
 #endif
