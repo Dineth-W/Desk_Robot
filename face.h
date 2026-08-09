873b5f2e2648ef440a37c419d23392e483132635
Dineth Wijesuriya <168741233+Dineth-W@users.noreply.github.com>
2026-08-09 19:22:43 +0530

    Add face expression enum and related functions

--- face.h
+++ face.h
@@ -1,7 +1,29 @@
 #ifndef FACE_H
 #define FACE_H
 
+#include <Arduino.h>
+
+// Face Expressions
+enum FaceExpression
+{
+    FACE_NORMAL,
+    FACE_HAPPY,
+    FACE_SAD,
+    FACE_SLEEP,
+    FACE_SURPRISED,
+    FACE_TALKING
+};
+
+// Initialization
 void faceInit();
+
+// Updates
 void updateFace();
 
+// Drawing
+void drawFace();
+
+// Expression Control
+void setExpression(FaceExpression expression);
+FaceExpression getExpression();
 #endif
