21ffc4a81a15dadf5263643cd8d94ff134b4d76e
Dineth Wijesuriya <168741233+Dineth-W@users.noreply.github.com>
2026-08-09 19:18:06 +0530

    Update clock.h

--- clock.h
+++ clock.h
@@ -1,7 +1,34 @@
 #ifndef CLOCK_H
 #define CLOCK_H
 
+#include <Arduino.h>
+
+//=============================================
+// Initialization
+//=============================================
+
 void clockInit();
+
+//=============================================
+// Updates
+//=============================================
+
 void updateClock();
+
+//=============================================
+// Drawing
+//=============================================
+
+void drawClock();
+
+//=============================================
+// Time Access
+//=============================================
+
+bool getCurrentTime(struct tm *timeinfo);
+
+String getTimeString();
+
+String getDateString();
 
 #endif
